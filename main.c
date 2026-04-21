9d1647eedbed96e0fa268f31eb5e4f017b996e96
Sajid <sajidhossain2606@gmail.com>
2026-04-22 04:41:01 +0600

    Fixed some bugs and added more options

--- main.c
+++ main.c
@@ -21,7 +21,9 @@ int main(){
         printf("\n1. Add a book\n");
         printf("\n2. Search a book\n");
         printf("\n3. Count book\n");
-        printf("\n4. Exit\n");\
+        printf("\n4. Remove a book\n");
+        printf("\n5. Show all available books\n");
+        printf("\n6. Exit\n");\
         printf("\n----------------------------------\n");
         printf("\nEnter your option: ");
         scanf("%d",&input);
@@ -41,10 +43,10 @@ int main(){
             break;
         case 2:
             // Code to search a book
+            int found=0;
             char searchTitle[100];
             printf("Enter the title of the book to search: ");
             scanf(" %[^\n]s", searchTitle); // Read the search title with spaces
-            int found = 0;
             for (int i = 0; i < count; i++) {
                 if (strcmp(books[i].title, searchTitle) == 0) {
                     printf("Book found: %s by %s\n", books[i].title, books[i].author);
@@ -63,6 +65,43 @@ int main(){
             printf("\n");
             break;
         case 4:
+            int got=0;
+            char search[100];
+            printf("Enter the name of book to remove:");
+            scanf(" %[^\n]",search);
+            for (int i = 0; i < count; i++) {
+            if (strcmp(books[i].title, search) == 0) {
+            // Shift all subsequent books left by one position
+            for (int j = i; j < count - 1; j++) {
+                strcpy(books[j].title, books[j + 1].title);
+                strcpy(books[j].author, books[j + 1].author);
+            }
+            got = 1;
+            count--;
+            printf("Book '%s' removed successfully.\n", search);
+            break;
+                 }
+             }
+
+            if (!got) {
+                printf("Book not found.\n");
+            }
+            break;
+        case 5:
+            // Show all available books
+            if (count == 0) {
+                printf("No books available in the library.\n");
+            } else {
+                printf("\n========== Available Books ==========\n");
+                for(int i = 0; i < count; i++){
+                    printf("---------------------------------\n");
+                    printf("Title: %s\nAuthor: %s\n", books[i].title, books[i].author);
+                    printf("---------------------------------\n");
+                }
+            }
+            printf("\n");
+            break; 
+        case 6:
             printf("Exiting the program...\n");
             exit(0);
         default:
